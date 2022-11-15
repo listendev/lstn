dd8b9b49176667434a314a11837f5e901336054d
Leonardo Di Donato <leodidonato@gmail.com>
2022-11-15 13:12:19 +0100

    update(cmd): give to the in subcommand a positional argument
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- cmd/in.go
+++ cmd/in.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,22 +17,44 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
+	"github.com/listendev/lstn/pkg/validate"
 	"github.com/spf13/cobra"
 )
 
-// inCmd represents the in command
+// inCmd represents the in subcommand
 var inCmd = &cobra.Command{
 	Use:   "in",
-	Short: "A brief description of your command",
+	Short: "Inspect your dependencies",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("in called")
+	Example: `  lstn in
+  lstn in .
+  lstn in /we/snitch
+  lstn in sub/dir`,
+	Args:              validateInArgs, // Executes before RunE
+	ValidArgsFunction: activeHelpIn,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// Obtain the target directory that we want to listen in
+		targetDir, err := getTargetDirectory(args)
+		if err != nil {
+			return fmt.Errorf("couldn't get to know on which directory you want me to listen in")
+		}
+		// Check that the target directory contains a package.json file
+		packageJSONErrors := validate.Singleton.Var(filepath.Join(targetDir, "package.json"), "file,json")
+		// NOTE > In the future, we can fallback to detect other package managers here rather than erroring out
+		if packageJSONErrors != nil {
+			return fmt.Errorf("couldn't find a package.json in %s", targetDir)
+		}
+
+		fmt.Println("in called", args)
+		return nil
 	},
 }
 
@@ -48,4 +70,54 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// inCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// getTargetDirectory computes the absolute
+// path from the input arguments.
+//
+// When no argument has been specified,
+// it return the current working directory.
+func getTargetDirectory(args []string) (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	if len(args) > 0 {
+		dir = args[0]
+	}
+
+	return filepath.Abs(dir)
+}
+
+// validateInArgs validates the input arguments.
+//
+// It checks that there is maximum one argument.
+// It checks that the argument is an existing directory, too.
+func validateInArgs(c *cobra.Command, args []string) error {
+	if err := cobra.MaximumNArgs(1)(c, args); err != nil {
+		return err
+	}
+	// No further validation left if there are no arguments at all
+	if len(args) == 0 {
+		return nil
+	}
+	if errs := validate.Singleton.Var(args[0], "dir"); errs != nil {
+		return fmt.Errorf("requires the argument to be an existing directory")
+	}
+
+	return nil
+}
+
+// TODO(leodido) > Double-check it's working
+func activeHelpIn(c *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	var comps []string
+	if len(args) == 0 {
+		comps = cobra.AppendActiveHelp(comps, "Executing against the current working directory")
+	} else if len(args) == 1 {
+		comps = cobra.AppendActiveHelp(comps, fmt.Sprintf("Executing against directory '%s'", args[0]))
+	} else {
+		comps = cobra.AppendActiveHelp(comps, "This command does not take any more arguments")
+	}
+	return comps, cobra.ShellCompDirectiveFilterDirs
 }
