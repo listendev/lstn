1b175c16e7e4c1a60e18a593ae425c2e1e4823a7
Leonardo Di Donato <leodidonato@gmail.com>
2022-11-24 14:04:57 +0100

    new(pkg/validate): setup a translator for validation errors
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- pkg/validate/validate.go
+++ pkg/validate/validate.go
@@ -1,14 +1,38 @@
 package validate
 
 import (
+	"reflect"
+	"strings"
+
+	en "github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
 )
+
+type ValidationErrors = validator.ValidationErrors
 
 // Singleton is the validator singleton instance.
 //
 // This way it caches the structs info.
 var Singleton *validator.Validate
 
+// Translator is the universal translator for validation errors.
+var Translator ut.Translator
+
 func init() {
 	Singleton = validator.New()
+
+	// Register a function to get the field name from "name" tags.
+	Singleton.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("name"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+
+	eng := en.New()
+	Translator, _ = ut.New(eng, eng).GetTranslator("en")
+	en_translations.RegisterDefaultTranslations(Singleton, Translator)
 }
