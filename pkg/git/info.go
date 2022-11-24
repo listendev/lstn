29b3ec58933d4c55430045fef0c05b5a762b2a8e
Leonardo Di Donato <leodidonato@gmail.com>
2022-11-24 16:34:45 +0100

    chore(pkg): add license headers
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- pkg/git/info.go
+++ pkg/git/info.go
@@ -1,3 +1,18 @@
+/*
+Copyright © 2022 The listen.dev team <engineering@garnet.ai>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package git
 
 import (
@@ -18,6 +33,12 @@ func NewGitContextFrom(path string) (*GitContext, error) {
 	c := &GitContext{
 		Remotes: remotes,
 	}
+
+	// FIXME
+	// The current implementation does not respect the Git config semantics:
+	// repo.Config() doesn't merge the user/system Git configuration.
+	// This may result in a few issues (eg., querying remote references).
+	// See https://github.com/go-git/go-git/issues/395
 
 	return c, nil
 }
