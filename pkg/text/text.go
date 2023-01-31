d17db19b50c0b0a550aca9267eb64bc174bbd0f3
Leonardo Di Donato <leodidonato@gmail.com>
2023-01-31 21:17:38 +0100

    new(pkg/text): create package containing little functions to manipulate text
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ pkg/text/text.go
@@ -0,0 +1,52 @@
+package text
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+func Dedent(s string) string {
+	lines := strings.Split(s, "\n")
+	minIndent := -1
+
+	for _, l := range lines {
+		if len(l) == 0 {
+			continue
+		}
+
+		indent := len(l) - len(strings.TrimLeft(l, " "))
+		if minIndent == -1 || indent < minIndent {
+			minIndent = indent
+		}
+	}
+
+	if minIndent <= 0 {
+		return s
+	}
+
+	var buf bytes.Buffer
+	for _, l := range lines {
+		fmt.Fprintln(&buf, strings.TrimPrefix(l, strings.Repeat(" ", minIndent)))
+	}
+	return strings.TrimSuffix(buf.String(), "\n")
+}
+
+// Indent inserts prefix at the beginning of each non-empty line of s.
+func Indent(s, prefix string) string {
+	return string(IndentBytes([]byte(s), []byte(prefix)))
+}
+
+// IndentBytes inserts prefix at the beginning of each non-empty line of b.
+func IndentBytes(b, prefix []byte) []byte {
+	var res []byte
+	bol := true
+	for _, c := range b {
+		if bol && c != '\n' {
+			res = append(res, prefix...)
+		}
+		res = append(res, c)
+		bol = c == '\n'
+	}
+	return res
+}
