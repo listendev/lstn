3cce83b87f3dbe385af71632560511c5bad85955
Leonardo Di Donato <leodidonato@gmail.com>
2023-02-01 01:55:24 +0100

    new(pkg/cmd/help): create the lstn help exit (help topic)
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- pkg/cmd/help/topics.go
+++ pkg/cmd/help/topics.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/MakeNowJust/heredoc"
 	"github.com/listendev/lstn/pkg/text"
 	"github.com/spf13/cobra"
 )
@@ -33,7 +34,23 @@ var Topics = map[string]map[string]string{
 	},
 	"exit": {
 		"short": "Details about the lstn exit codes",
-		"long":  "TODO exit",
+		"long": heredoc.Doc(`
+			The lstn CLI follows the usual conventions regarding exit codes.
+
+			Meaning:
+
+			* when a command completes successfully, the exit code will be 0
+
+			* when a command fails for any reason, the exit code will be 1
+
+			* when a command is running but gets cancelled, the exit code will be 2
+
+			* when a command meets an authentication issue, the exit code will be 4
+
+			Notice that it's possible that a particular command may have more exit codes,
+			so it's a good practice to check the docs for the specific command
+			in case you're relying on the exit codes to control some behaviour.
+		`),
 	},
 	"manual": {
 		"short": "A comprehensive reference of all the lstn commands",
