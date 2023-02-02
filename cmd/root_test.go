a12e2159e4fd49164cda8c034ee49c577976571d
Francesco Di Donato <didonato.fr@gmail.com>
2023-02-02 19:46:43 +0100

    new(root): test lstn conf output
    It has been required a little, but important implementation change:
    instead of writing the out directly to the stdout, it is needed to canalize it through
    cobra, via 'cmd.Printf'. Only in this way is it possible to make assertion about the out.
    
    Signed-off-by: Francesco Di Donato <didonato.fr@gmail.com>

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -25,20 +26,28 @@ const (
 
 type CmdHelpSuite struct {
 	suite.Suite
-	commands commandsMap
+	commands     commandsMap
+	expectedOuts expectedOutsMap
 }
 
 func (suite *CmdHelpSuite) SetupSuite() {
 	Boot(&BootOptions{run: false})
 
-	suite.commands = make(map[commandName]*cobra.Command)
+	suite.commands = make(commandsMap)
 	suite.commands[Root] = rootCmd
 
 	for _, command := range rootCmd.Commands() {
 		suite.commands[commandName(command.Name())] = command
 	}
 
-	fmt.Println(suite.commands)
+	suite.expectedOuts = make(expectedOutsMap)
+	b := "# lstn configuration file\n\n"
+	b += fmt.Sprintf("%s\n\n", "The `lstn` CLI looks for a configuration file .lstn.yaml in your `$HOME` directory when it starts.")
+	b += fmt.Sprintf("%s\n", "In this file you can set the values for the global `lstn` configurations.")
+	b += fmt.Sprintf("%s\n\n", "Anyways, notice that environment variables, and flags (if any) override the values in your configuration file.")
+	b += fmt.Sprintf("%s\n\n", "Here's an example of a configuration file (with the default values):")
+	b += "```yaml\nendpoint: http://127.0.0.1:3000\nloglevel: info\ntimeout: 60\n```\n"
+	suite.expectedOuts[Config] = b
 }
 
 func TestCmdSuites(t *testing.T) {
@@ -46,11 +55,17 @@ func TestCmdSuites(t *testing.T) {
 }
 
 func (suite *CmdHelpSuite) TestTopics() {
+	topics := []commandName{Config}
+	for _, topic := range topics {
+		out := execute(suite.T(), suite.commands[Root], topic)
+		require.Equal(suite.T(), suite.expectedOuts[topic], out)
+	}
 }
 
 // Utils
 
 type commandsMap map[commandName]*cobra.Command
+type expectedOutsMap map[commandName]string
 
 func (m commandsMap) String() string {
 	res := ""
@@ -62,22 +77,16 @@ func (m commandsMap) String() string {
 	return res
 }
 
-func execute(t *testing.T, c *cobra.Command, args ...string) (string, string) {
+func execute(t *testing.T, c *cobra.Command, sub commandName) string {
 	t.Helper()
 
-	stdout := bytes.NewBufferString("")
-	stderr := bytes.NewBufferString("")
-	c.SetOut(stdout)
-	c.SetErr(stderr)
-	c.SetArgs(args)
+	b := bytes.NewBufferString("")
+	c.SetOut(b)
+	c.SetArgs([]string{string(sub)})
 	c.Execute()
-	o, err := io.ReadAll(stdout)
+	out, err := io.ReadAll(b)
 	if err != nil {
-		panic(err)
+		t.Fatalf("Error during reading of %s %s", c.Name(), sub)
 	}
-	e, err := io.ReadAll(stderr)
-	if err != nil {
-		panic(err)
-	}
-	return string(o), string(e)
+	return string(out)
 }
