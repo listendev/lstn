807fc7daa97b9b4da5c848bd608f4579beace7ea
Francesco Di Donato <didonato.fr@gmail.com>
2023-02-02 17:25:08 +0100

    new(text): create first test for IndentBytes
    Fixes bug in implementation. It is required to initialize the var b to a non-nil value.
    
    Signed-off-by: Francesco Di Donato <didonato.fr@gmail.com>

--- pkg/text/text_test.go
+++ pkg/text/text_test.go
@@ -23,19 +23,24 @@ func (suite *TextSuite) TestDedent() {
 	}{}
 
 	for i, tc := range cases {
-		require.Equal(suite.T(), tc.expected, Dedent(tc.s), info{i, tc.s})
+		require.Equal(suite.T(), tc.expected, Dedent(tc.s), fmt.Sprintf("Index: %d\nOriginal:\n %v\n", i, tc.s))
 	}
 }
 
-func (suite *TextSuite) TestIndent() {}
-
-func (suite *TextSuite) TestIndentBytes() {}
-
-type info struct {
-	i        int
-	original interface{}
+func (suite *TextSuite) TestIndent() {
 }
 
-func (i info) String() string {
-	return fmt.Sprintf("Index: %d\nOriginal:\n %v\n", i.i, i.original)
+func (suite *TextSuite) TestIndentBytes() {
+	prefix := []byte("  ")
+	cases := []struct {
+		b        []byte
+		prefix   []byte
+		expected []byte
+	}{
+		{[]byte(""), prefix, []byte("")},
+	}
+
+	for i, tc := range cases {
+		require.Equal(suite.T(), tc.expected, IndentBytes(tc.b, tc.prefix), fmt.Sprintf("Index: %d\n", i))
+	}
 }
