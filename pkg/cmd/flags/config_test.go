14410433950877405fa5892f77271bacdbcf1bfe
Francesco Di Donato <didonato.fr@gmail.com>
2023-02-03 12:41:55 +0100

    new(flags/config): test utility functions
    Signed-off-by: Francesco Di Donato <didonato.fr@gmail.com>

--- pkg/cmd/flags/config_test.go
+++ pkg/cmd/flags/config_test.go
@@ -20,3 +20,35 @@ func (suite *FlagsConfigSuite) TestNewConfigOptions() {
 	assert.NoError(suite.T(), err)
 	assert.IsType(suite.T(), &ConfigOptions{}, i)
 }
+
+func (suite *FlagsConfigSuite) TestGetConfigFlagsNames() {
+	m := GetConfigFlagsNames()
+	assert.Equal(suite.T(), 3, len(m))
+
+	expected := make(map[string]string)
+	expected["loglevel"] = "LogLevel"
+	expected["endpoint"] = "Endpoint"
+	expected["timeout"] = "Timeout"
+
+	for k, v := range m {
+		e, ok := expected[k]
+		assert.True(suite.T(), ok)
+		assert.Equal(suite.T(), e, v)
+	}
+}
+
+func (suite *FlagsConfigSuite) TestGetConfigFlagsDefaults() {
+	m := GetConfigFlagsDefaults()
+	assert.Equal(suite.T(), 3, len(m))
+
+	expected := make(map[string]string)
+	expected["endpoint"] = "http://127.0.0.1:3000"
+	expected["loglevel"] = "info"
+	expected["timeout"] = "60"
+
+	for k, v := range m {
+		e, ok := expected[k]
+		assert.True(suite.T(), ok)
+		assert.Equal(suite.T(), e, v)
+	}
+}
