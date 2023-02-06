69d8131e2eb8b539f37a7ff7cd71f8b2a7b7b489
Francesco Di Donato <didonato.fr@gmail.com>
2023-02-06 13:06:20 +0100

    chore: run specific test for each test case
    Signed-off-by: Francesco Di Donato <didonato.fr@gmail.com>

--- pkg/cmd/flags/base_test.go
+++ pkg/cmd/flags/base_test.go
@@ -19,22 +19,27 @@ func TestFlagsBaseSuite(t *testing.T) {
 
 func (suite *FlagsBaseSuite) TestValidate() {
 	cases := []struct {
+		desc        string
 		o           Options
 		expectedStr []string
 	}{
 		{
+			"empty config options",
 			&ConfigOptions{},
 			[]string{"timeout must be 30 or greater", "endpoint must be a valid URL"},
 		},
 		{
+			"invalid timeout",
 			&ConfigOptions{Timeout: 29, Endpoint: "http://127.0.0.1:3000"},
 			[]string{"timeout must be 30 or greater"},
 		},
 		{
+			"invalid endpoint",
 			&ConfigOptions{Timeout: 31, Endpoint: "http://invalid.endpoint"},
 			[]string{"endpoint must be a valid listen.dev endpoint"},
 		},
 		{
+			"valid config options",
 			&ConfigOptions{Timeout: 31, Endpoint: "http://127.0.0.1:3000"},
 			[]string{},
 		},
@@ -42,20 +47,25 @@ func (suite *FlagsBaseSuite) TestValidate() {
 
 	for _, tc := range cases {
 		bo := new(baseOptions)
-		actual := bo.Validate(tc.o)
-		assert.Equal(suite.T(), len(tc.expectedStr), len(actual))
-		for _, a := range actual {
-			assert.Contains(suite.T(), tc.expectedStr, a.Error())
-		}
+
+		suite.T().Run(tc.desc, func(t *testing.T) {
+			actual := bo.Validate(tc.o)
+			assert.Equal(suite.T(), len(tc.expectedStr), len(actual))
+			for _, a := range actual {
+				assert.Contains(suite.T(), tc.expectedStr, a.Error())
+			}
+		})
 	}
 }
 
 func (suite *FlagsBaseSuite) TestTransform() {
 	cases := []struct {
+		desc     string
 		o        Options
 		expected error
 	}{
 		{
+			"empty config options",
 			&ConfigOptions{},
 			nil,
 		},
@@ -66,6 +76,9 @@ func (suite *FlagsBaseSuite) TestTransform() {
 	ctx := context.Background()
 	for _, tc := range cases {
 		bo := new(baseOptions)
+		suite.T().Run(tc.desc, func(t *testing.T) {
+			assert.Equal(t, tc.expected, bo.Transform(ctx, tc.o))
+		})
 		require.Equal(suite.T(), tc.expected, bo.Transform(ctx, tc.o))
 	}
 }
