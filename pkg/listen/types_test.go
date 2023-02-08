911871412864cf033e2c7e2b05978a5b04edec5c
Leonardo Di Donato <leodidonato@gmail.com>
2023-02-08 13:30:22 +0100

    new(pkg/listen): test that AnalysisRequest correctly marshals into JSON encoding the package-lock
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- pkg/listen/types_test.go
+++ pkg/listen/types_test.go
@@ -16,6 +16,7 @@
 package listen
 
 import (
+	"encoding/json"
 	"os"
 	"path"
 	"testing"
@@ -152,5 +153,79 @@ func TestNewAnalysisRequest(t *testing.T) {
 }
 
 func TestAnalysisRequestMarshal(t *testing.T) {
-	// TODO: implement
+	validPackageLockJSON, _ := npm.NewPackageLockJSONFromBytes([]byte(heredoc.Doc(`{
+		"name": "sample",
+		"version": "1.0.0",
+		"lockfileVersion": 3,
+		"requires": true,
+		"packages": {
+			"": {
+				"name": "sample",
+				"version": "1.0.0",
+				"license": "ISC",
+				"dependencies": {
+					"react": "18.0.0"
+				}
+			},
+			"node_modules/@babel/runtime": {
+				"version": "7.20.13",
+				"resolved": "https://registry.npmjs.org/@babel/runtime/-/runtime-7.20.13.tgz",
+				"integrity": "sha512-gt3PKXs0DBoL9xCvOIIZ2NEqAGZqHjAnmVbfQtB620V0uReIQutpel14KcneZuer7UioY8ALKZ7iocavvzTNFA==",
+				"dependencies": {
+					"regenerator-runtime": "^0.13.11"
+				},
+				"engines": {
+					"node": ">=6.9.0"
+				}
+			}
+		}
+	}`)))
+
+	validPackages := npm.Packages{
+		"@babel/runtime": npm.Package{Version: "7.20.13", Shasum: "7055ab8a7cff2b8f6058bf6ae45ff84ad2aded4b"},
+	}
+
+	validAnalysisRequest, err := NewAnalysisRequest(validPackageLockJSON, validPackages)
+	assert.Nil(t, err)
+
+	validPackageLockBody := []byte(`"ewoJIm5hbWUiOiAic2FtcGxlIiwKCSJ2ZXJzaW9uIjogIjEuMC4wIiwKCSJsb2NrZmlsZVZlcnNpb24iOiAzLAoJInJlcXVpcmVzIjogdHJ1ZSwKCSJwYWNrYWdlcyI6IHsKCQkiIjogewoJCQkibmFtZSI6ICJzYW1wbGUiLAoJCQkidmVyc2lvbiI6ICIxLjAuMCIsCgkJCSJsaWNlbnNlIjogIklTQyIsCgkJCSJkZXBlbmRlbmNpZXMiOiB7CgkJCQkicmVhY3QiOiAiMTguMC4wIgoJCQl9CgkJfSwKCQkibm9kZV9tb2R1bGVzL0BiYWJlbC9ydW50aW1lIjogewoJCQkidmVyc2lvbiI6ICI3LjIwLjEzIiwKCQkJInJlc29sdmVkIjogImh0dHBzOi8vcmVnaXN0cnkubnBtanMub3JnL0BiYWJlbC9ydW50aW1lLy0vcnVudGltZS03LjIwLjEzLnRneiIsCgkJCSJpbnRlZ3JpdHkiOiAic2hhNTEyLWd0M1BLWHMwREJvTDl4Q3ZPSUlaMk5FcUFHWnFIakFubVZiZlF0QjYyMFYwdVJlSVF1dHBlbDE0S2NuZVp1ZXI3VWlvWThBTEtaN2lvY2F2dnpUTkZBPT0iLAoJCQkiZGVwZW5kZW5jaWVzIjogewoJCQkJInJlZ2VuZXJhdG9yLXJ1bnRpbWUiOiAiXjAuMTMuMTEiCgkJCX0sCgkJCSJlbmdpbmVzIjogewoJCQkJIm5vZGUiOiAiPj02LjkuMCIKCQkJfQoJCX0KCX0KfQ=="`)
+
+	validPackagesBody := []byte(`{"@babel/runtime":{"version":"7.20.13","shasum":"7055ab8a7cff2b8f6058bf6ae45ff84ad2aded4b"}}`)
+
+	tests := []struct {
+		desc    string
+		areq    *AnalysisRequest
+		lock    []byte
+		pkgs    []byte
+		wantErr string
+	}{
+		{
+			desc:    "valid",
+			areq:    validAnalysisRequest,
+			lock:    validPackageLockBody,
+			pkgs:    validPackagesBody,
+			wantErr: "",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			res, err := json.Marshal(tc.areq)
+			if err != nil {
+				assert.Nil(t, res)
+				if assert.Error(t, err) {
+					assert.Equal(t, tc.wantErr, err.Error())
+				}
+			} else {
+				assert.Nil(t, err)
+
+				var o map[string]json.RawMessage
+				e := json.Unmarshal(res, &o)
+				assert.Nil(t, e)
+
+				assert.Equal(t, tc.lock, []byte(o["package-lock"]))
+				assert.Equal(t, tc.pkgs, []byte(o["packages"]))
+			}
+		})
+	}
 }
