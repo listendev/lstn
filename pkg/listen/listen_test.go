471b2850ae8b363217363e0e8a387b62559e7f6a
Leonardo Di Donato <leodidonato@gmail.com>
2023-02-14 02:16:35 +0100

    new(pkg/listen): test verdicts and analysis HTTP requests
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- pkg/listen/listen_test.go
+++ pkg/listen/listen_test.go
@@ -16,13 +16,21 @@
 package listen
 
 import (
+	"bytes"
 	"context"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/MakeNowJust/heredoc"
+	internaltesting "github.com/listendev/lstn/internal/testing"
 	"github.com/listendev/lstn/pkg/cmd/flags"
 	pkgcontext "github.com/listendev/lstn/pkg/context"
+	"github.com/listendev/lstn/pkg/npm"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 var (
@@ -44,7 +52,7 @@ func (ctx mockContextLocalEndpoint) Done() <-chan struct{} {
 }
 
 func (ctx mockContextLocalEndpoint) Err() error {
-	return context.DeadlineExceeded
+	return nil
 }
 
 func (ctx mockContextLocalEndpoint) Value(key interface{}) interface{} {
@@ -59,11 +67,50 @@ func (ctx mockContextLocalEndpoint) Value(key interface{}) interface{} {
 	return nil
 }
 
+type fakeRequest struct{}
+
+func (fr *fakeRequest) IsRequest() bool {
+	return true
+}
+
+func TestUnsupportedRequestType(t *testing.T) {
+	o, e := newOptions(WithContext(context.Background()))
+	assert.Nil(t, e)
+
+	_, err := getEndpointURLFromContext(&fakeRequest{}, o)
+	if assert.Error(t, err) {
+		assert.Equal(t, "unsupported request type", err.Error())
+	}
+}
+
+func TestMissingConfigurationOptions(t *testing.T) {
+	o, e := newOptions(WithContext(context.Background()))
+	assert.Nil(t, e)
+
+	if _, err := getEndpointURLFromContext(&AnalysisRequest{}, o); assert.Error(t, err) {
+		assert.Equal(t, "couldn't obtain configuration options", err.Error())
+	}
+
+	if _, err := getEndpointURLFromContext(&VerdictsRequest{}, o); assert.Error(t, err) {
+		assert.Equal(t, "couldn't obtain configuration options", err.Error())
+	}
+}
+
 func TestLocalEndpoint(t *testing.T) {
-	endpoint, err := getBaseURLFromContext(mockContextLocalEndpoint{})
+	o, e := newOptions(WithContext(mockContextLocalEndpoint{}))
+	assert.Nil(t, e)
+
+	endpointAnalysis, err := getEndpointURLFromContext(&AnalysisRequest{}, o)
 	assert.Nil(t, err)
-	assert.Equal(t, localEndpoint, endpoint)
-	assert.Equal(t, "/api/npm", getAPIPrefix(endpoint))
+	assert.True(t, strings.HasPrefix(endpointAnalysis, localEndpoint))
+	assert.Equal(t, "/api/npm", getAPIPrefix(endpointAnalysis))
+	assert.True(t, strings.HasSuffix(endpointAnalysis, "analysis"))
+
+	endpointVerdicts, err := getEndpointURLFromContext(&VerdictsRequest{}, o)
+	assert.Nil(t, err)
+	assert.True(t, strings.HasPrefix(endpointVerdicts, localEndpoint))
+	assert.Equal(t, "/api/npm", getAPIPrefix(endpointVerdicts))
+	assert.True(t, strings.HasSuffix(endpointVerdicts, "verdicts"))
 }
 
 type mockContextNonLocalEndpoint struct{}
@@ -96,8 +143,152 @@ func (ctx mockContextNonLocalEndpoint) Value(key interface{}) interface{} {
 }
 
 func TestNonLocalEndpoint(t *testing.T) {
-	endpoint, err := getBaseURLFromContext(mockContextNonLocalEndpoint{})
+	o, e := newOptions(WithContext(mockContextNonLocalEndpoint{}))
+	assert.Nil(t, e)
+
+	endpointAnalysis, err := getEndpointURLFromContext(&AnalysisRequest{}, o)
 	assert.Nil(t, err)
-	assert.Equal(t, nonLocalEndpoint, endpoint)
-	assert.Equal(t, "/api", getAPIPrefix(endpoint))
+	assert.True(t, strings.HasPrefix(endpointAnalysis, nonLocalEndpoint))
+	assert.Equal(t, "/api", getAPIPrefix(endpointAnalysis))
+	assert.True(t, strings.HasSuffix(endpointAnalysis, "analysis"))
+
+	endpointVerdicts, err := getEndpointURLFromContext(&VerdictsRequest{}, o)
+	assert.Nil(t, err)
+	assert.True(t, strings.HasPrefix(endpointVerdicts, nonLocalEndpoint))
+	assert.Equal(t, "/api", getAPIPrefix(endpointVerdicts))
+	assert.True(t, strings.HasSuffix(endpointVerdicts, "verdicts"))
+}
+
+type RequestsSuite struct {
+	suite.Suite
+	server *httptest.Server
+}
+
+func (suite *RequestsSuite) BeforeTest(suiteName, testName string) {
+	suite.Assert().Equal("RequestsSuite", suiteName)
+
+	switch testName {
+	case "TestAnalysisRequest":
+		resp := []byte(`[{"name":"js-tokens","shasum":"19203fb59991df98e3a287050d4647cdeaf32499","verdicts":[],"version":"4.0.0"},{"name":"loose-envify","shasum":"71ee51fa7be4caec1a63839f7e682d8132d30caf","verdicts":[],"version":"1.4.0"}]`)
+		suite.server = internaltesting.MockHTTPServer(suite.Assert(), "analysis", resp, http.StatusOK)
+	case "TestVerdictsRequest":
+		resp := []byte(`[{"name":"js-tokens","shasum":"19203fb59991df98e3a287050d4647cdeaf32499","verdicts":[],"version":"4.0.0"}]`)
+		suite.server = internaltesting.MockHTTPServer(suite.Assert(), "verdicts", resp, http.StatusOK)
+	}
+}
+
+func (suite *RequestsSuite) TearDownSuite() {
+	if suite.server != nil {
+		suite.server.Close()
+	}
+}
+
+func (suite *RequestsSuite) TestAnalysisRequestWithBackgroundContext() {
+	ctx := context.Background()
+	resp, data, erro := Packages(&AnalysisRequest{}, WithContext(ctx))
+	suite.Assert().Nil(resp)
+	suite.Assert().Nil(data)
+	if suite.Assert().Error(erro) {
+		suite.Assert().Equal("couldn't obtain configuration options", erro.Error())
+	}
+}
+
+func (suite *RequestsSuite) TestVerdictsRequestWithBackgroundContext() {
+	ctx := context.Background()
+	resp, data, erro := Packages(&VerdictsRequest{}, WithContext(ctx))
+	suite.Assert().Nil(resp)
+	suite.Assert().Nil(data)
+	if suite.Assert().Error(erro) {
+		suite.Assert().Equal("couldn't obtain configuration options", erro.Error())
+	}
+}
+
+func (suite *RequestsSuite) TestAnalysisRequestEmpty() {
+	resp, data, erro := Packages(&AnalysisRequest{}, WithContext(mockContextLocalEndpoint{}))
+	suite.Assert().Nil(resp)
+	suite.Assert().Nil(data)
+	if suite.Assert().Error(erro) {
+		suite.Assert().Contains(erro.Error(), "package lock is mandatory")
+	}
+}
+
+func (suite *RequestsSuite) TestVerdictsRequestEmpty() {
+	resp, data, erro := Packages(&VerdictsRequest{}, WithContext(mockContextLocalEndpoint{}))
+	suite.Assert().Nil(resp)
+	suite.Assert().Nil(data)
+	if suite.Assert().Error(erro) {
+		suite.Assert().Contains(erro.Error(), "name is mandatory")
+	}
+}
+
+func (suite *RequestsSuite) TestAnalysisRequest() {
+	plj, _ := npm.NewPackageLockJSONFromBytes([]byte(heredoc.Doc(`{
+		"name": "sample",
+		"version": "1.0.0",
+		"lockfileVersion": 1,
+		"requires": true,
+		"dependencies": {
+			"js-tokens": {
+				"version": "4.0.0",
+				"resolved": "https://registry.npmjs.org/js-tokens/-/js-tokens-4.0.0.tgz",
+				"integrity": "sha512-RdJUflcE3cUzKiMqQgsCu06FPu9UdIJO0beYbPhHN4k6apgJtifcoCtT9bcxOpYBtpD2kCM6Sbzg4CausW/PKQ=="
+			},
+			"loose-envify": {
+				"version": "1.4.0",
+				"resolved": "https://registry.npmjs.org/loose-envify/-/loose-envify-1.4.0.tgz",
+				"integrity": "sha512-lyuxPGr/Wfhrlem2CL/UcnUc1zcqKAImBDzukY7Y5F/yQiNdko6+fRLevlw1HgMySw7f611UIY408EtxRSoK3Q==",
+				"requires": {
+					"js-tokens": "^3.0.0 || ^4.0.0"
+				}
+			}
+		}
+	}`)))
+
+	pkg := npm.Packages{
+		"js-tokens":    npm.Package{Version: "4.0.0", Shasum: "19203fb59991df98e3a287050d4647cdeaf32499"},
+		"loose-envify": npm.Package{Version: "1.4.0", Shasum: "71ee51fa7be4caec1a63839f7e682d8132d30caf"},
+	}
+
+	req, err1 := NewAnalysisRequest(plj, pkg)
+	suite.Assert().Nil(err1)
+
+	res1, _, err1 := Packages(req, WithBaseURL(suite.server.URL))
+	suite.Assert().Nil(err1)
+
+	exp1 := &Response{
+		Package{Name: "js-tokens", Version: "4.0.0", Shasum: "19203fb59991df98e3a287050d4647cdeaf32499", Verdicts: []Verdict{}},
+		Package{Name: "loose-envify", Version: "1.4.0", Shasum: "71ee51fa7be4caec1a63839f7e682d8132d30caf", Verdicts: []Verdict{}},
+	}
+	suite.Assert().Equal(exp1, res1)
+
+	_, res2, err2 := Packages(req, WithBaseURL(suite.server.URL), WithJSONOptions(flags.JSONFlags{JSON: true}))
+	suite.Assert().Nil(err2)
+
+	exp2 := []byte(`[{"name":"js-tokens","shasum":"19203fb59991df98e3a287050d4647cdeaf32499","verdicts":[],"version":"4.0.0"},{"name":"loose-envify","shasum":"71ee51fa7be4caec1a63839f7e682d8132d30caf","verdicts":[],"version":"1.4.0"}]`)
+
+	suite.Assert().Equal(exp2, bytes.TrimSuffix(res2, []byte("\n")))
+}
+
+func (suite *RequestsSuite) TestVerdictsRequest() {
+	req, err1 := NewVerdictsRequest([]string{"js-tokens", "4.0.0", "19203fb59991df98e3a287050d4647cdeaf32499"})
+	suite.Assert().Nil(err1)
+
+	res1, _, err1 := Packages(req, WithBaseURL(suite.server.URL))
+	suite.Assert().Nil(err1)
+
+	exp1 := &Response{
+		Package{Name: "js-tokens", Version: "4.0.0", Shasum: "19203fb59991df98e3a287050d4647cdeaf32499", Verdicts: []Verdict{}},
+	}
+	suite.Assert().Equal(exp1, res1)
+
+	_, res2, err2 := Packages(req, WithBaseURL(suite.server.URL), WithJSONOptions(flags.JSONFlags{JSON: true}))
+	suite.Assert().Nil(err2)
+
+	exp2 := []byte(`[{"name":"js-tokens","shasum":"19203fb59991df98e3a287050d4647cdeaf32499","verdicts":[],"version":"4.0.0"}]`)
+
+	suite.Assert().Equal(exp2, bytes.TrimSuffix(res2, []byte("\n")))
+}
+
+func TestRequestsSuite(t *testing.T) {
+	suite.Run(t, new(RequestsSuite))
 }
