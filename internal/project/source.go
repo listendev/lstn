cdc2bc2e9a2d1ab064fd20e8998751e217a969b6
Leonardo Di Donato <leodidonato@gmail.com>
2023-02-17 19:51:29 +0100

    feat(internal/project): create GetSourceURL() to compute the GitHub URL of any source file by file name
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ internal/project/source.go
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright © 2023 The listen.dev team <engineering@garnet.ai>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package project
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+// GetSourceURL computes the URL for a given source file.
+func GetSourceURL(fileName string) string {
+	baseURL := fmt.Sprintf("%s/blob/%s", URL, DefaultBranch)
+	relative, err := filepath.Rel(BaseDir, fileName)
+	if err != nil {
+		return baseURL
+	}
+
+	return fmt.Sprintf("%s/%s", baseURL, relative)
+}
