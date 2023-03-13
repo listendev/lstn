5c160acd8d29ac41106df172fd9634e681ec18b6
Lorenzo Fontana <fontanalorenz@gmail.com>
2023-03-13 14:59:21 +0100

    test(pkg/cmd/packagesprinter): test print verdict

--- pkg/cmd/packagesprinter/table_test.go
+++ pkg/cmd/packagesprinter/table_test.go
@@ -5,32 +5,34 @@ import (
 	"testing"
 
 	"github.com/cli/cli/pkg/iostreams"
+	"github.com/listendev/lstn/pkg/listen"
+	"github.com/stretchr/testify/require"
 )
 
 func TestTablePrinter_printVerdictMetadata(t *testing.T) {
 	tests := []struct {
-		name         string
-		metadata     map[string]interface{}
-		wantedOutput string
+		name           string
+		metadata       map[string]interface{}
+		expectedOutput string
 	}{
 		{
-			name:         "empty metadata does not print anything",
-			metadata:     map[string]interface{}{},
-			wantedOutput: "",
+			name:           "empty metadata does not print anything",
+			metadata:       map[string]interface{}{},
+			expectedOutput: "",
 		},
 		{
 			name: "metadata with value nil does not print anything",
 			metadata: map[string]interface{}{
 				"key": nil,
 			},
-			wantedOutput: "",
+			expectedOutput: "",
 		},
 		{
 			name: "metadata with value prints the key and value",
 			metadata: map[string]interface{}{
 				"key": "myvalue",
 			},
-			wantedOutput: "    key: myvalue\n",
+			expectedOutput: "    key: myvalue\n",
 		},
 		{
 			name: "metadata with value prints the key and value while ignoring npm_package_name and npm_package_version",
@@ -39,7 +41,7 @@ func TestTablePrinter_printVerdictMetadata(t *testing.T) {
 				"npm_package_name":    "react",
 				"npm_package_version": "0.18.0",
 			},
-			wantedOutput: "    key: myvalue\n",
+			expectedOutput: "    key: myvalue\n",
 		},
 		{
 			name: "metadata with values prints the values it recognizes and ignores the rest",
@@ -48,7 +50,7 @@ func TestTablePrinter_printVerdictMetadata(t *testing.T) {
 				"myintkey":      10,
 				"somethingelse": float64(10.20),
 			},
-			wantedOutput: "    myintkey: 10\n    mystringkey: a string\n",
+			expectedOutput: "    myintkey: 10\n    mystringkey: a string\n",
 		},
 		{
 			name: "metadata with value prints the key and value while ignoring empty values",
@@ -58,25 +60,89 @@ func TestTablePrinter_printVerdictMetadata(t *testing.T) {
 				"commandline":     `sh -c  node -e "try{require('./_postinstall')}catch(e){}" || exit 0`,
 				"server_ip":       "",
 			},
-			wantedOutput: "    commandline: sh -c  node -e \"try{require('./_postinstall')}catch(e){}\" || exit 0\n    executable_path: /bin/sh\n    parent_name:: node\n",
+			expectedOutput: "    commandline: sh -c  node -e \"try{require('./_postinstall')}catch(e){}\" || exit 0\n    executable_path: /bin/sh\n    parent_name:: node\n",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			outBuf := &bytes.Buffer{}
-			errBuf := &bytes.Buffer{}
 			tr := &TablePrinter{
 
 				streams: &iostreams.IOStreams{
-					Out:    outBuf,
-					ErrOut: errBuf,
+					Out: outBuf,
 				},
 			}
 			tr.printVerdictMetadata(tt.metadata)
 
-			if tt.wantedOutput != outBuf.String() {
-				t.Errorf("wanted output %q, got %q", tt.wantedOutput, outBuf.String())
+			require.Equal(t, tt.expectedOutput, outBuf.String())
+		})
+	}
+}
+
+func TestTablePrinter_printVerdict(t *testing.T) {
+	tests := []struct {
+		name           string
+		p              *listen.Package
+		verdict        listen.Verdict
+		expectedOutput string
+	}{
+		{
+			name: "verdict with metadata prints the verdict and metadata",
+			p: &listen.Package{
+				Name:     "my-package",
+				Version:  "1.0.0",
+				Verdicts: []listen.Verdict{},
+
+				Problems: []listen.Problem{},
+			},
+			verdict: listen.Verdict{
+				Message:  "outbound network connection",
+				Priority: "high",
+				Metadata: map[string]interface{}{
+					"parent_name":     "node",
+					"executable_path": "/bin/sh",
+					"commandline":     `sh -c  node -e "try{require('./_postinstall')}catch(e){}" || exit 0`,
+					"server_ip":       "",
+				},
+			},
+			expectedOutput: "  [high] outbound network connection\n    commandline: sh -c  node -e \"try{require('./_postinstall')}catch(e){}\" || exit 0\n    executable_path: /bin/sh\n    parent_name: node\n",
+		},
+		{
+			name: "verdict with transitive metadata marks the verdict as transitive",
+			p: &listen.Package{
+				Name:     "my-package",
+				Version:  "1.0.0",
+				Verdicts: []listen.Verdict{},
+
+				Problems: []listen.Problem{},
+			},
+			verdict: listen.Verdict{
+				Message:  "outbound network connection",
+				Priority: "high",
+				Metadata: map[string]interface{}{
+					"npm_package_name":    "react",
+					"npm_package_version": "0.18.0",
+					"parent_name":         "node",
+					"executable_path":     "/bin/sh",
+					"commandline":         `sh -c  node -e "try{require('./_postinstall')}catch(e){}" || exit 0`,
+					"server_ip":           "",
+				},
+			},
+			expectedOutput: "  [high] outbound network connection (from transitive dependency react@0.18.0)\n    commandline: sh -c  node -e \"try{require('./_postinstall')}catch(e){}\" || exit 0\n    executable_path: /bin/sh\n    parent_name: node\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			outBuf := &bytes.Buffer{}
+			tr := &TablePrinter{
+
+				streams: &iostreams.IOStreams{
+					Out: outBuf,
+				},
 			}
+			tr.printVerdict(tt.p, tt.verdict)
+
+			require.Equal(t, tt.expectedOutput, outBuf.String())
 		})
 	}
 }
