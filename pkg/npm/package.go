8a877a86b5e5473d7a21e301c56cc7aad1ff0b34
Leonardo Di Donato <leodidonato@gmail.com>
2023-03-13 19:27:42 +0100

    feat(pkg/npm): create function to get the package.json as an io.Reader
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ pkg/npm/package.go
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright © 2023 The listen.dev team <engineering@garnet.ai>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package npm
+
+import (
+	"fmt"
+	"io"
+	"path/filepath"
+)
+
+func readPackageJSON(dir string) (io.Reader, error) {
+	f, err := activeFS.Open(filepath.Join(dir, "package.json"))
+	if err != nil {
+		return nil, fmt.Errorf("couldn't read the package.json file")
+	}
+
+	return f, nil
+}
