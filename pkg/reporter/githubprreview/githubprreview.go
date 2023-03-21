27f6149090a60c093ddc4dcc13cbda2e61693582
Lorenzo Fontana <fontanalorenz@gmail.com>
2023-03-21 22:44:58 +0100

    chore: make linter happy

--- pkg/reporter/githubprreview/githubprreview.go
+++ pkg/reporter/githubprreview/githubprreview.go
@@ -1,3 +1,18 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright © 2023 The listen.dev team <engineering@garnet.ai>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package githubprreview
 
 import (
@@ -46,7 +61,8 @@ func (r *ReviewReporter) stickyComment(owner string, repo string, id int, commen
 		return err
 	}
 
-	if _, err := io.Copy(&buf, comment); err != nil {
+	_, err = io.Copy(&buf, comment)
+	if err != nil {
 		return err
 	}
 
@@ -59,14 +75,17 @@ func (r *ReviewReporter) stickyComment(owner string, repo string, id int, commen
 	}
 	commentFn := func() error {
 		_, _, err = r.ghClient.Issues.CreateComment(r.ctx, owner, repo, id, issueComment)
+
 		return err
 	}
 	for _, comment := range comments {
 		if strings.HasPrefix(*comment.Body, stickyReviewCommentAnnotation) {
 			commentFn = func() error {
 				_, _, err = r.ghClient.Issues.EditComment(r.ctx, owner, repo, *comment.ID, issueComment)
+
 				return err
 			}
+
 			break
 		}
 	}
@@ -90,5 +109,6 @@ func (r *ReviewReporter) Report(req *request.Report) error {
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
