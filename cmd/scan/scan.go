8ef979b8df2f2f8db84e1d0c42e95aefe4f378e0
Leonardo Di Donato <leodidonato@gmail.com>
2023-03-21 21:03:59 +0100

    feat(cmd): support --debug-options flag that prints out (as JSON) the options and exits
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- cmd/scan/scan.go
+++ cmd/scan/scan.go
@@ -65,9 +65,6 @@ The verdicts it returns are listed by the name of each package and its specified
 		RunE: func(c *cobra.Command, args []string) error {
 			ctx = c.Context()
 
-			io := c.Context().Value(pkgcontext.IOStreamsKey).(*iostreams.IOStreams)
-			io.StartProgressIndicator()
-
 			// Obtain the local options from the context
 			opts, err := pkgcontext.GetOptionsFromContext(ctx, pkgcontext.ScanKey)
 			if err != nil {
@@ -77,6 +74,15 @@ The verdicts it returns are listed by the name of each package and its specified
 			if !ok {
 				return fmt.Errorf("couldn't obtain options for the current child command")
 			}
+
+			if scanOpts.DebugOptions {
+				c.Println(scanOpts.AsJSON())
+
+				return nil
+			}
+
+			io := c.Context().Value(pkgcontext.IOStreamsKey).(*iostreams.IOStreams)
+			io.StartProgressIndicator()
 
 			// Obtain the target directory that we want to listen in
 			targetDir, err := arguments.GetDirectory(args)
