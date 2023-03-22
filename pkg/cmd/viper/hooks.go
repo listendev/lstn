642da9f51efe7d3e3914590a3728f7b9173fd57d
Leonardo Di Donato <leodidonato@gmail.com>
2023-03-22 19:15:14 +0100

    feat(pkg/cmd/viper): create viper decoder for reporter types
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ pkg/cmd/viper/hooks.go
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright © 2023 The listen.dev team <engineering@garnet.ai>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package viper
+
+import (
+	"reflect"
+
+	"github.com/listendev/lstn/pkg/cmd"
+	"github.com/mitchellh/mapstructure"
+)
+
+func StringToReportType() mapstructure.DecodeHookFunc {
+	return func(f reflect.Type, t reflect.Type, data interface{}) (interface{}, error) {
+		if f.Kind() != reflect.String {
+			return data, nil
+		}
+		if t != reflect.TypeOf(cmd.AllReport) {
+			return data, nil
+		}
+
+		return cmd.ParseReportType(data.(string))
+	}
+}
