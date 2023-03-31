4323d273c43635bc684a4e020a0298f2ebb96fcd
Leonardo Di Donato <leodidonato@gmail.com>
2023-03-31 18:47:03 +0200

    chore: make linters happy
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- pkg/cmd/flags/base.go
+++ pkg/cmd/flags/base.go
@@ -114,8 +114,7 @@ func Define(c *cobra.Command, o interface{}, startingGroup string) {
 			c.Flags().IntVarP(ref, tag, short, val, descr)
 
 		case reflect.Slice:
-			switch f.Type.Elem().Kind() {
-			case reflect.String:
+			if f.Type.Elem().Kind() == reflect.String {
 				val := field.Interface().([]string)
 				ref := (*[]string)(unsafe.Pointer(field.UnsafeAddr()))
 				c.Flags().StringSliceVarP(ref, tag, short, val, descr)
