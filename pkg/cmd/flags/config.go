fd2809549f9cf6f96526120821749277f95db1e0
Leonardo Di Donato <leodidonato@gmail.com>
2024-03-22 19:23:05 +0100

    feat(pkg/cmd/flags): ensure token flags are not blank when passed in
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- pkg/cmd/flags/config.go
+++ pkg/cmd/flags/config.go
@@ -34,8 +34,8 @@ import (
 var _ cmd.Options = (*ConfigFlags)(nil)
 
 type Token struct {
-	GitHub string `name:"GitHub token" flag:"gh-token" desc:"set the GitHub token" flagset:"Token" json:"gh-token"`
-	JWT    string `name:"JWT token" flag:"jwt-token" desc:"set the listen.dev auth token" flagset:"Token" json:"jwt-token"`
+	GitHub string `name:"GitHub token" flag:"gh-token" desc:"set the GitHub token" flagset:"Token" json:"gh-token" validate:"omitempty,notblank"`
+	JWT    string `name:"JWT token" flag:"jwt-token" desc:"set the listen.dev auth token" flagset:"Token" json:"jwt-token" validate:"omitempty,notblank"`
 }
 
 type Registry struct {
