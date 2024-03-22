57a457c7e6381d5f5dd7dd768a206008219eb431
Leonardo Di Donato <leodidonato@gmail.com>
2024-03-22 14:11:24 +0100

    chore: make linters happy
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- pkg/reporter/pro/pro.go
+++ pkg/reporter/pro/pro.go
@@ -1,3 +1,18 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright © 2023 The listen.dev team <engineering@garnet.ai>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package pro
 
 import (
@@ -39,7 +54,7 @@ func New(ctx context.Context, opts ...reporter.Option) (reporter.Reporter, error
 }
 
 func (r *rep) Run(res listen.Response) error {
-	fmt.Println("TODO: for every listen.Package call the Dependency API")
+	fmt.Println("TODO: for every listen.Package call the Dependency API", len(res))
 
 	return nil
 }
@@ -48,7 +63,7 @@ func (r *rep) WithConfigOptions(opts *flags.ConfigFlags) {
 	r.opts = opts
 }
 
-func (r *rep) WithGitHubClient(client *github.Client) {
+func (r *rep) WithGitHubClient(_ *github.Client) {
 	// Do nothing
 }
 
