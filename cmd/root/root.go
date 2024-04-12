509c2ff593051173b98cefd1cd946e37cb7e6b0d
Leonardo Di Donato <leodidonato@gmail.com>
2024-04-12 18:32:17 +0200

    fix(cmd): specify ecosystem (NPM) for lstn to and lstn scan
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -344,7 +344,7 @@ func New(ctx context.Context) (*Command, error) {
 		}
 	}
 
-	// Fallback to the default subcommand when the user doesn't specify one explicitly.
+	// Fallback to the default subcommand (scan) when the user doesn't specify one explicitly.
 	c, _, err := rootCmd.Find(os.Args[1:])
 	if err == nil && c.Use == rootCmd.Use && c.Flags().Parse(os.Args[1:]) != pflag.ErrHelp {
 		args := append([]string{scanCmd.Name()}, os.Args[1:]...)
