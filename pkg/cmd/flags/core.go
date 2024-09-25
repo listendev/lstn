1ff1b0f0b8996b620af8385f428bf0e37a9bb6b4
Leonardo Di Donato <leodidonato@gmail.com>
2024-09-25 12:15:59 +0200

    feat(pkg/cmd): create flagset for Core related flags
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ pkg/cmd/flags/core.go
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright © 2023 The listen.dev team <engineering@garnet.ai>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package flags
+
+type CoreFlags struct {
+	BaseURL string `name:"Core API base URL" flag:"core-api" desc:"set a custom Core API base URL" validate:"omitempty,url" transform:"tsuffix=/" json:"core-api" default:"https://core.listen.dev"`
+}
