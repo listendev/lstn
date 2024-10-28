aa14a1bc58b2f1f3d651aca282246154d917d607
Leonardo Di Donato <leodidonato@gmail.com>
2024-10-28 21:32:47 +0100

    fix(cmd): remove --core-endpoint from lstn in|scan|to
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- cmd/scan/scan.go
+++ cmd/scan/scan.go
@@ -165,7 +165,7 @@ The verdicts it returns are listed by the name of each package and its specified
 	// scanCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Local flags will only run when this command is called directly
-	scanOpts.Attach(scanCmd, []string{"jwt-token", "lockfiles"})
+	scanOpts.Attach(scanCmd, []string{"jwt-token", "lockfiles", "core-endpoint"})
 
 	// Pass the options through the context
 	ctx = context.WithValue(ctx, pkgcontext.ScanKey, scanOpts)
