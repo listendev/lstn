1f7794d09887c14f2a5885c0c07f335d133224d9
Leonardo Di Donato <leodidonato@gmail.com>
2024-10-28 20:15:19 +0100

    feat(cmd/ci): make lstn ci a root command (for its part of the commands tree)
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- cmd/ci/ci.go
+++ cmd/ci/ci.go
@@ -1,40 +1,15 @@
-// SPDX-License-Identifier: Apache-2.0
-//
-// Copyright © 2023 The listen.dev team <engineering@garnet.ai>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//	http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 package ci
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"net/http"
-	"os"
-	"os/exec"
-	"path/filepath"
 	"runtime"
-	"strings"
 
-	"github.com/cli/cli/pkg/iostreams"
+	"github.com/listendev/lstn/cmd/ci/enable"
+	"github.com/listendev/lstn/cmd/ci/report"
 	"github.com/listendev/lstn/internal/project"
-	"github.com/listendev/lstn/pkg/ci"
-	"github.com/listendev/lstn/pkg/cmd/flags"
 	"github.com/listendev/lstn/pkg/cmd/groups"
 	"github.com/listendev/lstn/pkg/cmd/options"
 	pkgcontext "github.com/listendev/lstn/pkg/context"
-	"github.com/listendev/lstn/pkg/validate"
-	"github.com/listendev/pkg/apispec"
 	"github.com/spf13/cobra"
 )
 
@@ -43,193 +18,56 @@ var (
 )
 
 func New(ctx context.Context) (*cobra.Command, error) {
-	var ciCmd = &cobra.Command{
+	var c = &cobra.Command{
 		Use:                   "ci",
-		GroupID:               groups.Core.ID,
+		SilenceUsage:          true,
 		DisableFlagsInUseLine: true,
+		GroupID:               groups.Core.ID,
 		Short:                 "Listen in on what your CI does",
 		Long: `Eavesdrop everything happening under the hoods into your CI.
 
-Using this command, you can spy network and file activities occurring in your CI, whether it's your dependencies doing something shady or you.
-This command requires a listen.dev pro account.`,
+Using this set of commands, you can spy network and file activities occurring in your CI, whether it's your dependencies doing something shady or your code.
+
+A listen.dev pro is necessary.`,
 		Annotations: map[string]string{
 			"source": project.GetSourceURL(filename),
 		},
-		RunE: func(c *cobra.Command, _ []string) error {
-			ctx = c.Context()
-			// Obtain the local options from the context
-			opts, err := pkgcontext.GetOptionsFromContext(ctx, pkgcontext.CiKey)
-			if err != nil {
+		Args: func(c *cobra.Command, args []string) error {
+			if err := cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs)(c, args); err != nil {
 				return err
 			}
-			ciOpts, ok := opts.(*options.Ci)
-			if !ok {
-				return fmt.Errorf("couldn't obtain options for the current child command")
-			}
-
-			errs := []error{}
-			if err := validate.Singleton.Var(ciOpts.Token.GitHub, "mandatory"); err != nil {
-				tags, _ := flags.GetFieldTag(ciOpts, "ConfigFlags.Token.GitHub")
-				name, _ := tags.Lookup("name")
-				errs = append(errs, flags.Translate(err, name)...)
-			}
-			if err := validate.Singleton.Var(ciOpts.Token.JWT, "mandatory"); err != nil {
-				tags, _ := flags.GetFieldTag(ciOpts, "ConfigFlags.Token.JWT")
-				name, _ := tags.Lookup("name")
-				errs = append(errs, flags.Translate(err, name)...)
-			}
-			if len(errs) > 0 {
-				ret := "invalid configuration options/flags"
-				for _, e := range errs {
-					ret += "\n       "
-					ret += e.Error()
-				}
-
-				return fmt.Errorf(ret)
-			}
-
-			if ciOpts.DebugOptions {
-				c.Println(ciOpts.AsJSON())
-
-				return nil
-			}
-
-			// FIXME: exit if not on linux
-
-			info, infoErr := ci.NewInfo()
-			if infoErr != nil {
-				return fmt.Errorf("not running in a CI environment")
-			}
-
-			io := c.Context().Value(pkgcontext.IOStreamsKey).(*iostreams.IOStreams)
-			cs := io.ColorScheme()
-
-			// Block when running on fork pull requests
-			if info.HasReadOnlyGitHubToken() {
-				c.PrintErrln(cs.WarningIcon(), "lstn ci doesn not run on fork pull requests at the moment")
-
-				return nil
-			}
-
-			// Fetch settings from Core API
-			io.StartProgressIndicator()
-			coreClient, coreClientErr := apispec.NewClientWithResponses(
-				ciOpts.CoreFlags.BaseURL,
-				apispec.WithRequestEditorFn(func(_ context.Context, req *http.Request) error {
-					if req == nil {
-						io.StopProgressIndicator()
-						c.PrintErrln(cs.WarningIcon(), "empty settings request")
-
-						return fmt.Errorf("couldn't prepare the settings request for the Core API")
-					}
-					req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", ciOpts.Token.JWT))
-
-					return nil
-				}),
-			)
-			if coreClientErr != nil {
-				io.StopProgressIndicator()
-
-				return coreClientErr
-			}
-			coreResponse, coreResponseErr := coreClient.GetApiV1SettingsWithResponse(ctx)
-			if coreResponseErr != nil {
-				io.StopProgressIndicator()
-
-				return coreResponseErr
-			}
-			if coreResponse.StatusCode() != http.StatusOK {
-				io.StopProgressIndicator()
-				c.PrintErrln(cs.WarningIcon(), "settings request to Core API didn't work out", cs.Redf("(%d)", coreResponse.StatusCode()))
-
-				return fmt.Errorf("status code is not %d", http.StatusOK)
-			}
-			if coreResponse.JSON200 == nil {
-				io.StopProgressIndicator()
-				c.PrintErrln(cs.WarningIcon(), "empty settings response")
-
-				return fmt.Errorf("got empty settings from the Core API")
-			}
-			coreSettings := *coreResponse.JSON200
-			io.StopProgressIndicator()
-			c.Println(cs.SuccessIcon(), "Fetch settings")
-
-			// Create configuration for runtime eavesdropping tool
-			io.StartProgressIndicator()
-			envConfig := fmt.Sprintf("%s\n%s\n%s=%s\n%s=%s\n", strings.Join(coreSettings.TokensSlice(), "\n"), info.Dump(), "LISTENDEV_TOKEN", ciOpts.Token.JWT, "GITHUB_TOKEN", ciOpts.Token.GitHub)
-			envDirErr := os.MkdirAll("/var/run/jibril", 0750)
-			if envDirErr != nil {
-				io.StopProgressIndicator()
-
-				return envDirErr
-			}
-
-			envConfigFilename := "/var/run/jibril/default"
-			if err := os.WriteFile(envConfigFilename, []byte(envConfig), 0640); err != nil {
-				io.StopProgressIndicator()
-
-				return err
-			}
-			io.StopProgressIndicator()
-			c.Println(cs.SuccessIcon(), "Wrote config", cs.Magenta(envConfigFilename))
-
-			io.StartProgressIndicator()
-			var jibril *exec.Cmd
-			if len(ciOpts.Directory) > 0 {
-				file := filepath.Join(ciOpts.Directory, "jibril")
-				info, err := os.Stat(file)
-				if os.IsNotExist(err) {
-					io.StopProgressIndicator()
-
-					return fmt.Errorf("couldn't find the jibril binary in %s", ciOpts.Directory)
-				}
-				if info.IsDir() {
-					io.StopProgressIndicator()
-
-					return fmt.Errorf("expecting %s to be an executable file", file)
-				}
-				jibril = exec.CommandContext(ctx, file, "-s", "enable-now")
-			} else {
-				exe, err := exec.LookPath("jibril")
-				if err != nil {
-					io.StopProgressIndicator()
-
-					return fmt.Errorf("couldn't find the jibril executable in the PATH")
-				}
-				jibril = exec.CommandContext(ctx, exe, "-s", "enable-now")
-			}
-			io.StopProgressIndicator()
-			c.Println(cs.Blue("•"), "Install and enable", cs.Magenta(jibril.String()))
-
-			io.StartProgressIndicator()
-			jibrilOut, jibrilErr := jibril.CombinedOutput()
-			if jibrilErr != nil {
-				io.StopProgressIndicator()
-
-				return fmt.Errorf("couldn't install and enable jibril: %w", jibrilErr)
-			}
-			io.StopProgressIndicator()
-			c.Println(string(bytes.Trim(jibrilOut, "\n")))
 
 			return nil
 		},
+		ValidArgs: []string{"enable", "report"},
+		Run: func(c *cobra.Command, args []string) {
+		},
 	}
 
-	// Obtain the local options
-	ciOpts, err := options.NewCi()
+	// Attach `enable` child command
+	enableCmd, err := enable.New(ctx)
 	if err != nil {
 		return nil, err
 	}
+	c.AddCommand(enableCmd)
 
-	// Persistent flags here will work for this command and all subcommands
-	// ciCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// Attach `report` child command
+	reportCmd, err := report.New(ctx)
+	if err != nil {
+		return nil, err
+	}
+	c.AddCommand(reportCmd)
 
-	// Local flags will only run when this command is called directly
-	ciOpts.Attach(ciCmd, []string{"--ignore-packages", "--ignore-deptypes", "--select", "lockfiles", "npm-endpoint", "pypi-endpoint", "reporter", "npm-registry", "gh-owner", "gh-pull-id", "gh-repo"})
+	// Create the local options
+	emptyOpts, err := options.NewEmpty()
+	if err != nil {
+		return nil, err
+	}
+	emptyOpts.Attach(c, []string{})
 
 	// Pass the options through the context
-	ctx = context.WithValue(ctx, pkgcontext.CiKey, ciOpts)
-	ciCmd.SetContext(ctx)
+	ctx = context.WithValue(ctx, pkgcontext.EmptyKey, emptyOpts)
+	c.SetContext(ctx)
 
-	return ciCmd, nil
+	return c, nil
 }
